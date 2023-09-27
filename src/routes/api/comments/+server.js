88a469731aa10bae93b7dc24e3d9e3c8f287c974
brian <liar88828@gmail.com>
2023-09-27 20:35:45 +0700

    19 API Post Request

--- src/routes/api/comments/+server.js
+++ src/routes/api/comments/+server.js
@@ -11,3 +11,16 @@ export function GET ()
   // )
   return json( comments );
 }
+
+
+export async function POST ( requestEvent )
+{
+  const { request } = requestEvent;
+  const { text } = await request.json()
+  const newComment = {
+    id: comments.length + 1,
+    text
+  }
+  comments.push( newComment )
+  return json( comments, { status: 201 } );
+}
