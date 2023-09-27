1ef1710d348e45c5fb78bdbe9e3c044c3e9f6026
brian <liar88828@gmail.com>
2023-09-27 20:47:36 +0700

    21 API Patch Request

--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -8,3 +8,14 @@ export function GET ( requestEvent )
   const comment = comments.find( c => c.id === parseInt( commentId ) )
   return json( comment )
 }
+
+export async function PATCH ( requestEvent ) 
+{
+  const { params, request } = requestEvent
+  const { text } = await request.json()
+  const { commentId } = params
+  const comment = comments.find( c => c.id === parseInt( commentId ) )
+  comment.text = text
+  return json( comment )
+}
+
