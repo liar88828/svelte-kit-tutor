143f673483305294a8f965be76563d77a576e19f
brian <liar88828@gmail.com>
2023-09-27 20:42:16 +0700

    20 Dynamic API routes

--- src/routes/api/comments/+server.js
+++ src/routes/api/comments/+server.js
@@ -24,3 +24,4 @@ export async function POST ( requestEvent )
   comments.push( newComment )
   return json( comments, { status: 201 } );
 }
+
---
+++ src/routes/api/comments/[commentId]/+server.js
@@ -0,0 +1,10 @@
+import { comments } from "$lib/comment";
+import { json } from "@sveltejs/kit";
+
+export function GET ( requestEvent ) 
+{
+  const { params } = requestEvent
+  const { commentId } = params
+  const comment = comments.find( c => c.id === parseInt( commentId ) )
+  return json( comment )
+}
