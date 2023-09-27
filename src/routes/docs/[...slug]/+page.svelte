55e34c6b6708d2f576e1c544fe4bb184197e4d23
brian <liar88828@gmail.com>
2023-09-27 14:50:32 +0700

    8 Catch All Route

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+	const slugArg = $page.params.slug;
+	const slug = slugArg.split('/');
+</script>
+
+{#if slug.length === 1}
+	<h1>Docs home page {slug[0]}</h1>
+{/if}
