e3b5daf1d2a1f103d809c01ff4b99a4ed3c2bf65
brian <liar88828@gmail.com>
2023-09-27 14:59:36 +0700

    9 Optionals Parameter

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { page } from "$app/stores";
+	const greeting = {
+		en: "Hello",
+		es: "Hola",
+		fa: "Bonjour"
+	};
+
+	const { lang = "en" } = $page.params;
+	const greet = greeting[lang];
+</script>
+
+<h1>{greet}</h1>
