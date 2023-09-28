9952a2ab0485a6278c4862d4d38cdd260e83a300
brian <liar88828@gmail.com>
2023-09-28 15:54:18 +0700

    40 Preload Programmatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { goto, beforeNavigate, afterNavigate } from "$app/navigation";
+	import { goto, beforeNavigate, afterNavigate, preloadData, preloadCOde } from "$app/navigation";
 
 	const handleClick = () => {
 		console.log("placing your order");
@@ -10,18 +10,22 @@
 		console.log({ before: navigate });
 	});
 
-  
 	afterNavigate((navigate) => {
 		console.log({ after: navigate });
 	});
-  
 </script>
 
 <h1>welcome home</h1>
 <a href="/blog">Blog</a>
 <a href="/products">Products</a>
 <!-- <a href="/item" data-sveltekit-preload-code="hover">Item</a> -->
-<a href="/item" data-sveltekit-scroll >Item</a>
+<a href="/item" data-sveltekit-scroll>Item</a>
+<button
+	on:focus={async () => await preloadData("/item")}
+	on:mouseover={async () => await preloadData("/item")}
+	on:mouseover={async () => await preloadData("/item")}
+	on:click={() => goto("/item")}>Goto Item</button
+>
 <!-- <a href="/item" data-sveltekit-preload-data="hover">Item</a> -->
 <!-- <a href="/item" data-sveltekit-preload-data="tab">Item</a> -->
 <!-- <a href="/item" data-sveltekit-preload-data="off">Item</a> -->
