c0262f2aa0cb631bf64d1bcc0242af24b7b35d05
brian <liar88828@gmail.com>
2023-09-28 20:13:57 +0700

    49 Named Form actions

--- src/routes/auth/+page.server.js
+++ src/routes/auth/+page.server.js
@@ -1,7 +1,7 @@
 import { json } from "@sveltejs/kit"
 
 export const actions = {
-  default: async ( { request, cookies } ) =>
+  login: async ( { request, cookies } ) =>
   {
     const data = await request.formData()
     const username = data.get( "username" )
@@ -15,6 +15,21 @@ export const actions = {
     }
     cookies.set( "username", username, { path: "/" } )
     return { message: "logged in" }
+  },
 
+  register: async ( { request, cookies } ) =>
+  {
+    const data = await request.formData()
+    const username = data.get( "username" )
+    const password = data.get( "password" )
+
+    if ( !username || !password )
+    {
+      return json( {
+        message: "Missing username and password"
+      } )
+    }
+    cookies.set( "register", username, { path: "/" } )
+    return { message: "Success Register..." }
   }
 }
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -19,14 +19,14 @@
 
 <!-- for +server.js -->
 <!-- <form on:submit|preventDefault={handlerSubmit}> -->
-<form method="post">
-	<p>{form?.message||""}</p>
+<form method="post" action="?/login">
+	<p>{form?.message || ""}</p>
 	<!--  ------- for +server.js -->
 	<!-- <input type="text" name="username" placeholder="Username" bind:value={username} /> 
 	<input type="password" name="password" placeholder="Password" bind:value={password} />  -->
 	<!--  -->
-	<!--  -->
 	<input type="text" name="username" placeholder="Username" />
 	<input type="password" name="password" placeholder="Password" />
 	<button type="submit">Login</button>
+	<button formaction="?/register">Register</button>
 </form>
