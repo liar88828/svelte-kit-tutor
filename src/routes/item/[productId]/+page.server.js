be002688fca24dc5ad83330f21064324f5542b69
brian <liar88828@gmail.com>
2023-09-28 11:25:09 +0700

    30 Errors and RRedirects

--- src/routes/item/[productId]/+page.server.js
+++ src/routes/item/[productId]/+page.server.js
@@ -1,9 +1,18 @@
+import { error, redirect } from "@sveltejs/kit"
 
 export const load = async ( serverLoadEvent ) =>
 {
   const { fetch, params } = serverLoadEvent
   const { productId } = params
-  console.log( productId );
+  if ( productId > 3 )
+  {
+    // throw new Error( `Product is not found` )
+    // throw error( 404, {
+    //   message: `Product is not found`,
+    //   hint: "try a different product"
+    // } )
+    throw redirect( 307, "/item" )
+  }
   const res = await fetch( `http://localhost:3000/items/${ productId }` )
   const items = await res.json()
   console.log( items );
