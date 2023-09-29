1c06afcc0fee1c921661976c9abd179295533959
brian <liar88828@gmail.com>
2023-09-29 17:47:50 +0700

    55 Server only module

--- src/routes/contact/+page.js
+++ src/routes/contact/+page.js
@@ -1,5 +1,6 @@
 
 import { PUBLIC_BASE_URL_DB } from "$env/static/public"
+// import { secretKey } from "$lib/server/secrets"; //error because is on server side/component
 export const load = () =>
 {
   console.log( `Connecting to database... user ${ PUBLIC_BASE_URL_DB } ` );
--- src/routes/news/+page.server.js
+++ src/routes/news/+page.server.js
@@ -1,8 +1,12 @@
 import { redirect } from "@sveltejs/kit";
+import { secretKey } from "$lib/server/secrets";
+// can because is on server side
+
 export const load = ( { cookies, url }
 ) =>
 {
-  console.log( `Connecting to database... user ${ DB_USER } and pass ${ DB_PASSWORD }` );
+  console.log( secretKey );
+  // console.log( `Connecting to database... user ${ DB_USER } and pass ${ DB_PASSWORD }` );
   if ( !cookies.get( "username" ) )
   {
     throw redirect( 307, `/auth?redirectTo=${ url.pathname }` )
